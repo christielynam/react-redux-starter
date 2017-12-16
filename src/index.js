492eec9797a1c485ae5efd6e61cf89c7fc42f02c
Christie Lynam <christielynam@gmail.com>
2017-12-16 11:39:00 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker();
